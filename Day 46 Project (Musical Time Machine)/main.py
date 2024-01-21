5857549efb4d963d38cb7a0dd8709a88f7386af3
Elijah Joel S. Cañarejo <ejcanarejo21@gmail.com>
2024-01-21 21:14:23 +0800

    day 46 learning about Beautiful Soup, and Spotify API for project "Musical Time Machine"

--- Day 46 Project (Musical Time Machine)/main.py
+++ Day 46 Project (Musical Time Machine)/main.py
@@ -8,34 +8,39 @@ CLIENT_ID = ["YOUR_CLIENT_ID"]
 CLIENT_SECRET = ["YOUR_CLIENT_SECRET"]
 USERNAME = ["YOUR_SPOTIFY_USERNAME"]
 
-BILLBOARD_URL = 'https://www.billboard.com/charts/hot-100/2000-08-12/'
+BILLBOARD_URL = 'https://www.billboard.com/charts/hot-100'
+# 2000-08-12
 
-response = requests.get(url=BILLBOARD_URL)
+date = input("Which year do you want to travel to? Type the date in this format, YYYY-MM-DD: ")
+
+response = requests.get(url=f"{BILLBOARD_URL}/" + date)
 billboard_web = response.text
 
 soup = BeautifulSoup(billboard_web, "html.parser")
 
 
 songs = soup.findAll(name="li", class_="o-chart-results-list__item // lrv-u-flex-grow-1 lrv-u-flex lrv-u-flex-direction-column lrv-u-justify-content-center lrv-u-border-b-1 u-border-b-0@mobile-max lrv-u-border-color-grey-light lrv-u-padding-l-050 lrv-u-padding-l-1@mobile-max")
+song_number_one = soup.find("h3").getText().strip()
 song_titles = [title.find("h3").getText().strip() for title in songs]
+song_titles.insert(0,song_number_one)
 
-print(song_titles)
-print(len(song_titles))
-
-
-
-
-
-
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri="http://example.com",
                                                scope="playlist-modify-private",
                                                username=USERNAME))
 
 
-year_to_go = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD ")
+results = spotify.current_user()
+USER_ID = results['id']
+
+song_uris = [spotify.search(title)['tracks']['items'][0]['uri'] for title in song_titles]
+
+PLAYLIST_ID = spotify.user_playlist_create(user=USER_ID, name=f"{date} Billboard 100", public="false", description=f"Top 100 Billboard songs of {date}")['id']
+
+# add songs  to the playlist
+
+spotify.playlist_add_items(PLAYLIST_ID, items=song_uris)
 
 
 
